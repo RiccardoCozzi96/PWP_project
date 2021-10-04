8d7ca5160d3a8dfb722db093a0216973d3675562
AlessandroLiscio <aleliscio97@gmail.com>
2021-10-04 16:38:53 +0200

    results_viewer correctly sorting

--- results_viewer.py
+++ results_viewer.py
@@ -1,11 +1,16 @@
 import json, os, pandas as pd
 
 logs = {
-    "cp_base": json.load(open("./CP/base/log.json")), 
+    "cp_base": json.load(open("./CP/base/log.json")),
     "cp_general": json.load(open("./CP/general/log.json")),
     # "smt_base": json.load(open("./SMT/base/log.json")),
     # "smt_general": json.load(open("./SMT/general/log.json")),
     # "new": json.load(open("./sandbox/general/log.json")),
 }
 
-print(pd.DataFrame(logs))
+logs_df = pd.DataFrame(logs)
+
+idxs = logs_df.index.values.tolist()
+idxs = idxs[-2:] + idxs[:-2]
+
+print(logs_df.reindex(idxs))
